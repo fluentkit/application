89435fffcd397c09f8799b828c65ea436dda0db8
Lee Mason <lee.mason@gambitnash.co.uk>
2020-01-26 22:32:26 +0000

    chore(admin): Add panel field descriptions above panel bodies

--- resources/js/admin/components/elements/field/panel.vue
+++ resources/js/admin/components/elements/field/panel.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
         <fk-admin-title>{{ field.label }}</fk-admin-title>
+        <p v-if="field.description" class="text-gray-600 italic mb-4">{{ field.description }}</p>
         <fk-admin-panel>
-            <fk-admin-field-row v-for="field in field.fields" :key="field.id" :field="field" :value="value[field.id]" @input="updateValue(field.id, $event)"/>
+            <fk-admin-field-row
+                v-for="field in field.fields"
+                :key="field.id"
+                :field="field"
+                :value="value[field.id]"
+                @input="updateValue(field.id, $event)"
+            />
         </fk-admin-panel>
     </div>
 </template>
