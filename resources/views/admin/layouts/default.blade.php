c8656752776e4eefd5b34ddaf3de69724ff33ac4
Lee Mason <lee.mason@gambitnash.co.uk>
2020-04-09 20:05:44 +0100

    chore(admin): Use app name as page title

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>FluentKit</title>
+        <title>{{ config('app.name') }}</title>
         <link rel="stylesheet" href="{{ mix('/css/admin/admin.css') }}" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i&display=swap" />
