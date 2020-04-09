43e82a357e0a241f9c0bc329e06df46ba58ee649
Lee Mason <lee.mason@gambitnash.co.uk>
2020-04-09 22:26:36 +0100

    fix(app): Recursive merge original config with db supplied config

--- app/App.php
+++ app/App.php
@@ -50,8 +50,28 @@ class App extends Model
         $this->settings()
             ->get(['setting', 'value'])
             ->each(function (Setting $setting) {
+                if (is_array($setting->value)) {
+                    $default = config()->get($setting->setting, []);
+                    config()->set($setting->setting, $this->array_merge_recursive_distinct($default, $setting->value));
+                    return;
+                }
                 config()->set($setting->setting, $setting->value);
             });
+    }
+
+    private function array_merge_recursive_distinct(array $array1, array $array2)
+    {
+        $merged = $array1;
+
+        foreach ($array2 as $key => $value) {
+            if (is_array($value) && isset($merged[$key]) && is_array($merged [$key])) {
+                $merged[$key] = array_merge_recursive_distinct($merged [$key], $value);
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+
+        return $merged;
     }
 
     public static function setCurrent(App $app)
